1f3f39c81b86b62d6b94bd087eb3eabfa9e930c2
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2022-03-29 19:30:36 +0200

    provide GCC style mangled lib functions when MSVC++ is used

--- lib/str.cc
+++ lib/str.cc
@@ -556,7 +556,7 @@ namespace volvox {
 		free(s);
 		return n == bytes_to_write;
 	}
-
+#ifndef _MSC_VER
 	bool fprint(int fd, const char* pre, const RtType* ft, ... /* int w, int p, unsigned flags, val, char* post */) {
 		va_list ap;
 		va_start(ap, ft);
@@ -588,5 +588,46 @@ namespace volvox {
 		va_end(ap);
 		return has_succeeded;
 	}
+#endif
+}
+
+#if defined (_MSC_VER)
+// Volvox uses the Itanium mangling scheme for global symbols
+// which is also used by C++ compilers on most Unix-like systems
+// MSVC++ used it's own mangling scheme - however, if we use this
+// compiler we can provide mangled functions as "C" functions
+extern "C" {
+	bool _ZN6volvox6fprintEiPKcPKNS_6RtTypeEz(int fd, const char* pre, const volvox::RtType* ft, ... /* int w, int p, unsigned flags, val, char* post */) {
+		va_list ap;
+		va_start(ap, ft);
+		bool has_succeeded = volvox::vfprint(fd, false, pre, ft, ap);
+		va_end(ap);
+		return has_succeeded;
+	}
+
+	bool _ZN6volvox8fprintlnEiPKcPKNS_6RtTypeEz(int fd, const char* pre, const volvox::RtType* ft, ... /* int w, int p, unsigned flags, val, char* post */) {
+		va_list ap;
+		va_start(ap, ft);
+		bool has_succeeded = volvox::vfprint(fd, true, pre, ft, ap);
+		va_end(ap);
+		return has_succeeded;
+	}
+
+	bool _ZN6volvox5printEPKcPKNS_6RtTypeEz(const char* pre, const volvox::RtType* ft, ... /* int w, int p, unsigned flags, val, char* post */) {
+		va_list ap;
+		va_start(ap, ft);
+		bool has_succeeded = volvox::vfprint(1, false, pre, ft, ap);
+		va_end(ap);
+		return has_succeeded;
+	}
+
+	bool _ZN6volvox7printlnEPKcPKNS_6RtTypeEz(const char* pre, const volvox::RtType* ft, ... /* int w, int p, unsigned flags, val, char* post */) {
+		va_list ap;
+		va_start(ap, ft);
+		bool has_succeeded = volvox::vfprint(1, true, pre, ft, ap);
+		va_end(ap);
+		return has_succeeded;
+	}
 
 }
+#endif
