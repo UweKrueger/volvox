ed65452a317de040e19f529c5e941bef147f5da4
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2022-06-14 12:34:38 +0200

    make RT size arrays work

--- src/codegen.cc
+++ src/codegen.cc
@@ -111,24 +111,22 @@ llvm::Value *FixedArrayExprAST::codegen_raw() {
 	llvm::Value* LenVal = nullptr;
 	auto array_type = llvm::dyn_cast<llvm::ArrayType>(ft->type);
 	llvm::StructType* struct_type;
-	if (!array_type)
-		if ((struct_type = llvm::dyn_cast<llvm::StructType>(ft->type)))
-			if (struct_type->getNumElements() == 2 && struct_type->getElementType(0)->isIntegerTy()) {
-				array_type = llvm::dyn_cast<llvm::ArrayType>(struct_type->getElementType(1));
-				LenVal = Builder->CreateIntCast(Len->codegen(), llvm::Type::getInt64Ty(Context), false);
-				if (!LenVal) {
-					errs() << Len->Loc << ": cannot generate code for array length\n";
-					return nullptr;
-				}
-			}
 	if (!array_type) {
-		errs() << "Internal compiler error\n";
-		abort();
-	} 
+		errs() << Loc << ": internal error: array literal not of array type\n";
+		return nullptr;
+	}
 	uint64_t len = array_type->getNumElements();
-	std::vector<llvm::Constant*> Initializers;
+	if (!len)
+		if (Len)
+			LenVal = Builder->CreateIntCast(Len->codegen(), llvm::Type::getInt64Ty(Context), false);
+		else
+			LenVal = Builder->getInt64(Elements.size());
+	else
+		if (Len)
+			errs() << Loc << ": internal inconsistency CT RT array size are both given\n";
 	uint64_t i = 0;
-	llvm::Value* ini = llvm::UndefValue::get(array_type);
+	llvm::Type* initializer_type = llvm::ArrayType::get(ft->elem_type->type, Elements.size());
+	llvm::Value* ini = llvm::UndefValue::get(initializer_type);
 	for (auto& e: Elements) {
 		if (e) {
 			ini = Builder->CreateInsertValue(ini, Elem_convs[i](e->codegen_raw()), i, "arrlit");
@@ -137,11 +135,10 @@ llvm::Value *FixedArrayExprAST::codegen_raw() {
 		}
 		i++;
 	}
-	for(; i < len;  i++)
-		ini = Builder->CreateInsertValue(ini, llvm::Constant::getNullValue(ft->elem_type->type), i, "arrlit");
 	if (!LenVal) {
 		return ini;
 	} else {
+		llvm::Type* struct_type = llvm::StructType::get(llvm::Type::getInt64Ty(Context), initializer_type);
 		llvm::Value* varini = llvm::UndefValue::get(struct_type);
 		varini = Builder->CreateInsertValue(varini, Builder->CreateIntCast(LenVal, llvm::Type::getInt64Ty(Context), false), 0, "arrlen");
 		varini = Builder->CreateInsertValue(varini, ini, 1, "arrbeg");
