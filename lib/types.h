1cea8f4ca01be0f5eed6c0f3b9b9ef7e7212a8b8
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2022-07-19 22:09:39 +0200

    minor cleanup

--- lib/types.h
+++ lib/types.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "map.h"
 #include <inttypes.h>
 
 // Type Attributes
--- src/make.bat
+++ src/make.bat
@@ -9,7 +9,7 @@ del /q *.obj ..\volvox.* libvolvox.exp ..\lib\libvolvox.lib ..\libvolvox.dll
 @for %%f in (codegen.cc token.cc lexer.cc parser.cc types.cc main.cc mangler.cc) do @call comp.bat %%f %%~nf.obj /D"UNICODE" /D"_UNICODE"
 @for %%f in (..\wineditline\editline.c ..\wineditline\history.c ..\wineditline\fn_complete.c getopt.c) do @call comp.bat %%f %%~nf.obj /TC
 @for %%f in (..\lib\map.cc ..\lib\str.cc ..\lib\emutls.c) do @call comp.bat %%f %%~nf.obj /DLL /TC /D"LLVM_VERSION_MAJOR=14"
-@for %%f in (..\lib\array.cc) do clang++ -O2 -target x86_64-pc-windows-gnu -fno-exceptions -o array.obj -c -o %%~nf.obj %%f
+@for %%f in (..\lib\array.cc) do clang++ -O2 -target x86_64-pc-windows-gnu -fno-exceptions -c -o %%~nf.obj %%f
 link /DLL /out:libvolvox.dll str.obj map.obj array.obj
 
 move libvolvox.dll ..
