82dbc23e79b3945fdd2a49884786e97776c5e700
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2022-12-22 20:45:38 +0100

    fix filename save for locations (followup for 2d6df96)

--- src/lexer.cc
+++ src/lexer.cc
@@ -352,8 +352,8 @@ bool Lexer::next_input_file() {
 		}
 	}
 	if (source_index.back() < source_files.back().size()) {
-		Loc.File = source_files.back()[source_index.back()++].c_str();
-		SourceFileNames.push_back(Loc.File);
+		SourceFileNames.push_back(source_files.back()[source_index.back()++]);
+		Loc.File = SourceFileNames.back().c_str();
 		input_fd = open(Loc.File, O_CLOEXEC);
 		if (input_fd < 0) {
 			errs() << llvm::format("Cannot open input file \"%s\": %s\n", Loc.File, strerror(errno));
