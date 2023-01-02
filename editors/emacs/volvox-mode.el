fa43c8007b9feda74ae46df5a010a4fae4bb3671
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-01-02 22:53:24 +0100

    do some renames for volvox-mode

--- editors/emacs/volvox-mode.el
+++ editors/emacs/volvox-mode.el
@@ -1,4 +1,4 @@
-;;; bat-mode.el --- Major mode for editing DOS/Windows scripts  -*- lexical-binding: t -*-
+;;; volvox-mode.el --- Major mode for editing Volvox files  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2003, 2008-2022 Free Software Foundation, Inc.
 
@@ -28,7 +28,7 @@
 ;; groups for highlighting are:
 ;;
 ;; Face                          Example
-;; bat-label-face                :LABEL
+;; volvox-label-face             :LABEL
 ;; font-lock-comment-face        rem
 ;; font-lock-builtin-face        copy
 ;; font-lock-keyword-face        goto
@@ -39,7 +39,7 @@
 ;;
 ;; Usage:
 ;;
-;; See documentation of function `bat-mode'.
+;; See documentation of function `volvox-mode'.
 ;;
 ;; Separate package `dos-indent' (Matthew Fidler) provides rudimentary
 ;; indentation, see https://www.emacswiki.org/emacs/dos-indent.el.
@@ -52,36 +52,30 @@
 
 ;; 1  Preamble
 
-(defgroup bat-mode nil
+(defgroup volvox-mode nil
   "Major mode for editing DOS/Windows batch files."
   :link '(custom-group-link :tag "Font Lock Faces group" font-lock-faces)
   :group 'languages)
 
 ;; 2  User variables
 
-(defface bat-label-face '((t :weight bold))
-  "Font Lock mode face used to highlight labels in batch files.")
+(defface volvox-label-face '((t :weight bold))
+  "Font Lock mode face used to highlight labels in volvoxch files.")
 
 ;; 3  Internal variables
 
-(defvar bat-font-lock-keywords
+(defvar volvox-font-lock-keywords
   (eval-when-compile
     (let ((COMMANDS
-           '("assoc" "at" "attrib" "cd" "cls" "color" "copy" "date" "del" "dir"
-             "doskey" "echo" "endlocal" "erase" "fc" "find" "findstr" "format"
-             "ftype" "label" "md" "mkdir" "more" "move" "net" "path" "pause"
-             "popd" "prompt" "pushd" "rd" "ren" "rename" "replace" "rmdir" "set"
-             "setlocal" "shift" "sort" "subst" "time" "title" "tree" "type"
-             "ver" "vol" "xcopy"))
+           '("i16" "i32" "i64" "i8" "int" "real" "size_t" "ssize_t" "string" "u16" "u32" "u64" "u8"))
           (CONTROLFLOW
-           '("call" "cmd" "defined" "do" "else" "equ" "exist" "exit" "for" "geq"
-             "goto" "gtr" "if" "in" "leq" "lss" "neq" "not" "start"))
+           '("else" "fn" "if" "repeat" "return" "until" "while"))
           (UNIX
-           '("bash" "cat" "cp" "fgrep" "grep" "ls" "sed" "sh" "mv" "rm")))
+           '("const" "cdecl" "decl" "inline")))
       `(("\\_<\\(call\\|goto\\)\\_>[ \t]+%?\\([A-Za-z0-9_\\:.-]+\\)%?"
          (2 font-lock-constant-face t))
         ("^:[^:].*"
-         . 'bat-label-face)
+         . 'volvox-label-face)
         ("\\_<\\(defined\\|set\\)\\_>[ \t]*\\(\\(\\sw\\|\\s_\\)+\\)"
          (2 font-lock-variable-name-face))
         ("%~\\([0-9]\\)"
@@ -101,93 +95,108 @@
         (,(concat "\\_<" (regexp-opt UNIX) "\\_>")
          . font-lock-warning-face)))))
 
-(defvar bat-menu
-  '("Bat"
-    ["Run" bat-run :help "Run script"]
-    ["Run with Args" bat-run-args :help "Run script with args"]
+(defvar volvox-menu
+  '("Volvox"
+    ["Run" volvox-run :help "Run script"]
+    ["Run with Args" volvox-run-args :help "Run script with args"]
     "--"
     ["Imenu" imenu :help "Navigate with imenu"]
     "--"
-    ["Template" bat-template :help "Insert template"]
+    ["Template" volvox-template :help "Insert template"]
     "--"
-    ["Help (Command)" bat-cmd-help :help "Show help page for DOS command"]))
+    ["Help (Command)" volvox-cmd-help :help "Show help page for DOS command"]))
 
-(defvar bat-mode-map
+(defvar volvox-mode-map
   (let ((map (make-sparse-keymap)))
-    (easy-menu-define nil map nil bat-menu)
-    (define-key map [?\C-c ?\C-/] 'bat-cmd-help) ;FIXME: Why not C-c C-? ?
-    (define-key map [?\C-c ?\C-a] 'bat-run-args)
-    (define-key map [?\C-c ?\C-c] 'bat-run)
-    (define-key map [?\C-c ?\C-t] 'bat-template)
-    (define-key map [?\C-c ?\C-v] 'bat-run)
+    (easy-menu-define nil map nil volvox-menu)
+    (define-key map [?\C-c ?\C-/] 'volvox-cmd-help) ;FIXME: Why not C-c C-? ?
+    (define-key map [?\C-c ?\C-a] 'volvox-run-args)
+    (define-key map [?\C-c ?\C-c] 'volvox-run)
+    (define-key map [?\C-c ?\C-t] 'volvox-template)
+    (define-key map [?\C-c ?\C-v] 'volvox-run)
     map))
 
-(defvar bat-mode-syntax-table
+(defvar volvox-mode-syntax-table
   (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?\n ">" table)
-    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?\# "<" table)
+    (modify-syntax-entry ?\n ">#" table)
+    (modify-syntax-entry ?\" "\"\"" table)
     ;; Beware: `w' should not be used for non-alphabetic chars.
+    (modify-syntax-entry ?_ "_" table)
+    (modify-syntax-entry ?: "." table)
+    (modify-syntax-entry ?< "." table)
+    (modify-syntax-entry ?> "." table)
+    (modify-syntax-entry ?& "." table)
+    (modify-syntax-entry ?| "." table)
+    (modify-syntax-entry ?% "." table)
+    (modify-syntax-entry ?= "." table)
+    (modify-syntax-entry ?/ "." table)
+    (modify-syntax-entry ?+ "." table)
+    (modify-syntax-entry ?* "." table)
+    (modify-syntax-entry ?- "." table)
+    (modify-syntax-entry ?\; "." table)
+    (modify-syntax-entry ?\( "()" table)
+    (modify-syntax-entry ?\) ")(" table)
+    (modify-syntax-entry ?\{ "(}" table)
+    (modify-syntax-entry ?\} "){" table)
+    (modify-syntax-entry ?\[ "(]" table)
+    (modify-syntax-entry ?\] ")[" table)
     (modify-syntax-entry ?~ "_" table)
     (modify-syntax-entry ?% "." table)
     (modify-syntax-entry ?- "_" table)
     (modify-syntax-entry ?_ "_" table)
-    ;; FIXME: { and } can appear in identifiers?  Really?
-    (modify-syntax-entry ?{ "_" table)
-    (modify-syntax-entry ?} "_" table)
-    (modify-syntax-entry ?\\ "." table)
-    (modify-syntax-entry ?= "." table)
     table))
 
-(defconst bat--syntax-propertize
+(defconst volvox--syntax-propertize
   (syntax-propertize-rules
    ("^[ \t]*\\(?:\\(@?r\\)em\\_>\\|\\(?1::\\):\\).*" (1 "<"))))
 
 ;; 4  User functions
 
-(defun bat-cmd-help (cmd)
-  "Show help for batch file command CMD."
+(defun volvox-cmd-help (cmd)
+  "Show help for volvoxch file command CMD."
   (interactive "sHelp: ")
   (if (string-equal cmd "net")
       ;; FIXME: liable to quoting nightmare.  Use call-process?
       (shell-command "net /?") (shell-command (concat "help " cmd))))
 
-(defun bat-run ()
-  "Run a batch file."
+(defun volvox-run ()
+  "Run a volvoxch file."
   (interactive)
   ;; FIXME: liable to quoting nightmare.  Use call/start-process?
   (save-buffer) (shell-command buffer-file-name))
 
-(defun bat-run-args (args)
-  "Run a batch file with ARGS."
+(defun volvox-run-args (args)
+  "Run a volvoxch file with ARGS."
   (interactive "sArgs: ")
   ;; FIXME: Use `compile'?
   (shell-command (concat buffer-file-name " " args)))
 
-(defun bat-template ()
-  "Insert minimal batch file template."
+(defun volvox-template ()
+  "Insert minimal volvox file template."
   (interactive)
-  (goto-char (point-min)) (insert "@echo off\nsetlocal\n\n"))
+  (goto-char (point-min)) (insert "#\n# Program\n# Author\n\n"))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.\\(bat\\|cmd\\)\\'" . bat-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(volvox\\|vx\\)\\'" . volvox-mode))
 
 ;; 5  Main function
 
 ;;;###autoload
-(define-derived-mode bat-mode prog-mode "Bat"
-  "Major mode for editing DOS/Windows batch files.
-Start a new script from `bat-template'.  Read help pages for DOS commands
-with `bat-cmd-help'.  Navigate between sections using `imenu'.
-Run script using `bat-run' and `bat-run-args'.\n
-\\{bat-mode-map}"
+(define-derived-mode volvox-mode prog-mode "Volvox"
+  "Major mode for editing Volvox files.
+Start a new script from `volvox-template'.  Read help pages for DOS commands
+with `volvox-cmd-help'.  Navigate between sections using `imenu'.
+Run script using `volvox-run' and `volvox-run-args'.\n
+\\{volvox-mode-map}"
   (setq-local comment-start "rem ")
   (setq-local comment-start-skip "rem[ \t]+")
-  (setq-local syntax-propertize-function bat--syntax-propertize)
+  (setq-local syntax-propertize-function volvox--syntax-propertize)
   (setq-local font-lock-defaults
-       '(bat-font-lock-keywords nil t)) ; case-insensitive keywords
+       '(volvox-font-lock-keywords nil t)) ; case-insensitive keywords
   (setq-local imenu-generic-expression '((nil "^:[^:].*" 0)))
   (setq-local outline-regexp ":[^:]"))
 
-(provide 'bat-mode)
+(provide 'volvox-mode)
 
-;;; bat-mode.el ends here
+;;; volvox-mode.el ends here
