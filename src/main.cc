e0852a75764b32ce93aa481ee315609db0a3a84d
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-02-03 20:27:42 +0100

    provide arch.isJIT

--- src/main.cc
+++ src/main.cc
@@ -268,6 +268,18 @@ void init(const llvm::Triple& triple) {
 		errs() << "cannot create const " << "__OS_Bitwidth" << '\n';
 		abort();
 	}
+	FullVar is_jit_fv = {
+		.val = llvm::ConstantInt::get(llvm::Type::getInt1Ty(Context), comp_mode == comp_jit),
+		.mangled_name = strdup("__LLVM_JIT"),
+		.ft = {
+			.type = llvm::Type::getInt1Ty(Context),
+			.type_attr = A_rvalue | A_global | A_const | A_mainvar,
+		}
+	};
+	if (!lex.module->globals_table.insert("__LLVM_JIT", is_jit_fv)) {
+		errs() << "cannot create const " << "__LLVM_JIT" << '\n';
+		abort();
+	}
 }
 
 //===----------------------------------------------------------------------===//
