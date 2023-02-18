a433db2f45e84ad5a31759c6f22242d157ecabc1
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-02-18 15:18:16 +0100

    update copyright notice

--- src/global.h
+++ src/global.h
@@ -1,6 +1,6 @@
 #pragma once
 /*
- * Copyright © Uwe Krüger 2021, 2022
+ * Copyright © Uwe Krüger 2021, 2022, 2023
  * Licensed under the Apache License, Version 2.0
  * see file LICENSE or https://www.apache.org/licenses/LICENSE-2.0.txt
  */
