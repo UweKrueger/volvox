b401143a1f3d2ac5d27deb5f11ac5522e9d4e70f
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-10-23 22:40:37 +0200

    add Windows installer for Kate configuration

---
+++ editors/kde-syntax/install.bat
@@ -0,0 +1,4 @@
+mkdir %USERPROFILE%\AppData\Local\katepart5\script\indentation
+copy volvox.js %USERPROFILE%\AppData\Local\katepart5\script\indentation
+mkdir %USERPROFILE%\AppData\Local\katepart5\syntax
+copy volvox.xml %USERPROFILE%\AppData\Local\katepart5\syntax
