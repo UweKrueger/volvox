6d22d0642d9d5c03a87437d8b57e9ccccdf967d0
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-10-24 11:01:46 +0200

    rename 'kate' dir and add Unix installer

---
+++ editors/kate/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p "${HOME}/.local/share/katepart5/script/indentation"
+cp volvox.js "${HOME}/.local/share/katepart5/script/indentation"
+
+mkdir -p "${HOME}/.local/share/katepart5/syntax"
+cp volvox.xml "${HOME}/.local/share/katepart5/syntax"
