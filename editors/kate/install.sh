97245b2194a9d147013cd230c70a97caf0025436
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-11-26 11:51:57 +0100

    make Kate support file installation more robust

--- editors/kate/install.sh
+++ editors/kate/install.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+FILE_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
 mkdir -p "${HOME}/.local/share/katepart5/script/indentation"
-cp volvox.js "${HOME}/.local/share/katepart5/script/indentation"
+status=$?
+if [ $status != 0 ]; then
+	exit $status
+fi
+
+cp -v "${FILE_DIR}/volvox.js" "${HOME}/.local/share/katepart5/script/indentation"
+status=$?
+if [ $status != 0 ]; then
+	exit $status
+fi
 
 mkdir -p "${HOME}/.local/share/katepart5/syntax"
-cp volvox.xml "${HOME}/.local/share/katepart5/syntax"
+status=$?
+if [ $status != 0 ]; then
+	exit $status
+fi
+
+
+cp -v "${FILE_DIR}/volvox.xml" "${HOME}/.local/share/katepart5/syntax"
+status=$?
+if [ $status != 0 ]; then
+	exit $status
+fi
