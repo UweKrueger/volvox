8712bdab1b8a43953524f6c98a1dce6ba05a51a2
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-11-26 20:35:57 +0100

    avoid duplicate backslash

--- editors/kate/install.bat
+++ editors/kate/install.bat
@@ -1,4 +1,4 @@
 mkdir "%USERPROFILE%\AppData\Local\katepart5\script\indentation"
-copy "%~dp0\volvox.js" "%USERPROFILE%\AppData\Local\katepart5\script\indentation"
+copy "%~dp0volvox.js" "%USERPROFILE%\AppData\Local\katepart5\script\indentation"
 mkdir "%USERPROFILE%\AppData\Local\katepart5\syntax"
-copy "%~dp0\volvox.xml" "%USERPROFILE%\AppData\Local\katepart5\syntax"
+copy "%~dp0volvox.xml" "%USERPROFILE%\AppData\Local\katepart5\syntax"
