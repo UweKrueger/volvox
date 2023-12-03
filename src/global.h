b35bab6a3932e92b193b120790870ef1f3ca8373
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2023-12-03 22:37:10 +0100

    update Windows build to LLVM-17.0.6

--- src/global.h
+++ src/global.h
@@ -5,7 +5,7 @@
  * see file LICENSE or https://www.apache.org/licenses/LICENSE-2.0.txt
  */
 #ifdef _WIN32
-#include "../include/volvox-15.hh"
+#include "../include/volvox-17.hh"
 #include <windows.h>
 #include <synchapi.h>
 #include <processthreadsapi.h>
