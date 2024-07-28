825910adee7b5134c9e302a3c68fbb626d5fb419
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2024-07-29 00:45:34 +0200

    support interface type to empty interface

--- src/codegen.cc
+++ src/codegen.cc
@@ -644,9 +644,27 @@ llvm::Value* FunctionExprAST::codegen_raw(llvm::Value* target) {
 llvm::Value* InterfaceExprAST::codegen_raw(llvm::Value* target, bool strict) {
 	llvm::Value* val = nullptr;
 	llvm::Value* strict_target;
+	llvm::Constant* vtable = nullptr;
+	llvm::Value* rttype_ptr = nullptr;
 	if (!expr->ft->type)
 		return nullptr;
-	if (auto array_type = llvm::dyn_cast<llvm::ArrayType>(expr->ft->type)) {
+	if (expr->ft->type_attr & A_interface) {
+		auto inter_face = expr->codegen_raw();
+		rttype_ptr = Builder->CreateExtractValue(inter_face, 0);
+		llvm::Value* Offset;
+		if (ft->InterfaceProtos) {
+			// TODO: get Offset from vtable comparison
+			return nullptr;
+		} else {
+			// target type is empty interface -> completely skip vtable
+			Offset = Builder->CreateLoad(llvm_size_type, rttype_ptr);
+		}
+		val = Builder->CreateExtractValue(inter_face, 1);
+		rttype_ptr = Builder->CreateIntToPtr(
+			Builder->CreateAdd(
+				Builder->CreatePtrToInt(rttype_ptr, llvm_size_type), Offset), llvm_ptr_type);
+		goto have_rttype;	                   
+	} else if (auto array_type = llvm::dyn_cast<llvm::ArrayType>(expr->ft->type)) {
 		if (interface_ft) {
 			errs() << Loc << ": specialized interfaces cannot be implemented by arrays\n";
 			return nullptr;
@@ -748,17 +766,17 @@ llvm::Value* InterfaceExprAST::codegen_raw(llvm::Value* target, bool strict) {
 	}
 	if (!val)
 		return nullptr;
-	llvm::Constant* vtable = nullptr;
 	if (interface_ft && interface_ft->InterfaceProtos->first->getNumElements() > 1) {
 		vtable = getInterfaceVtable(Loc, expr->ft, interface_ft);
 		if (!vtable)
 			return nullptr;
 	}
-	llvm::Constant* rttype_ptr = getRtType(expr->ft, vtable);
+	rttype_ptr = getRtType(expr->ft, vtable);
 	if (!rttype_ptr) {
 		errs() << Loc << ": error creating type information for interface expression\n";
 		return nullptr;
 	}
+have_rttype:
 	llvm::Type* struct_type;
 	if (interface_ft) {
 		struct_type = interface_ft->type;
