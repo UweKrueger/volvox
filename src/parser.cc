64db59b5fc72143fdb48e6c155a77ddc5fcc65f0
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2026-06-29 10:08:44 +0200

    switch order of vec.insert arguments: pos, val - as in C++

--- src/parser.cc
+++ src/parser.cc
@@ -1863,8 +1863,10 @@ std::unique_ptr<ExprAST> getSelect(SourceLocation Loc, std::unique_ptr<ExprAST> 
 				(*vec_protos)[0]->ArgTypes.erase((*vec_protos)[0]->ArgTypes.begin() + 1);
 				(*vec_protos)[0]->ArgAttrs.erase((*vec_protos)[0]->ArgAttrs.begin() + 1);
 				(*vec_protos)[0]->ArgPos.erase((*vec_protos)[0]->ArgPos.begin() + 1);
-				if (real_method == "__push" || real_method == "__insert") {
+				if (real_method == "__push") {
 					(*vec_protos)[0]->ArgTypes[1] = new_FullType(*LHS->ft->elem_type, A_ref);
+				} else if (real_method == "__insert") {
+					(*vec_protos)[0]->ArgTypes[2] = new_FullType(*LHS->ft->elem_type, A_ref);
 				} else {
 					(*vec_protos)[0]->RetType = void_type;
 				}
