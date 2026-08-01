9b9bb3d0be8156a8ea8a23133c65acf025678258
Uwe Krüger <45282134+UweKrueger@users.noreply.github.com>
2026-08-01 11:35:26 +0200

    fix memory leak concerning mangled variable name

--- src/global.h
+++ src/global.h
@@ -743,9 +743,7 @@ struct FullVar {
 		}
 	}
 	void destroy() { // we cannot call it "~FullVar()" because it must not be called automatically
-		if (val) {
-			free((void*)this->mangled_name);
-		}
+		free((void*)this->mangled_name);
 		free((void*)this->possible_references);
 		/* delete var_usage_markers;
 		   - this is done at end of main() using "all_usage_markers" */
